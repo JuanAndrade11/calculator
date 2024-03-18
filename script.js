b6f16264a0bb18e3b17b8197a636de999ad628ae
JuanAndrade11 <juanpablo.andradem@gmail.com>
2024-03-18 16:37:01 -0500

    Add Display function with button logic (display only)

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let numA = 0, numB = 0, operator = '+', display = '';
+let numA = 0, numB = 0, operator = '+'
+let numbers = '0123456789';
+
+const display = document.querySelector('.display');
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function (event) {
+    if (event.target.classList.contains('button')) {
+        const buttonText = event.target.textContent;
+        displayText(buttonText);
+    }
+});
 
 function add(a, b) {
     return a + b;
@@ -26,5 +37,52 @@ function operate(a, b, operator) {
         case '*': return multiply(a, b);
         case '/': return divide(a, b);
         default: throw new Error('Unsupported operator');
+    }
+}
+
+function triggerTextBlinkEffect() {
+    const originalColor = display.style.color;
+    const backgroundColor = 'aquamarine'
+    display.style.color = backgroundColor;
+
+    setTimeout(() => {
+        display.style.color = originalColor;
+    }, 100);
+}
+
+function addText(text) {
+    display.textContent += text;
+}
+
+function toggleNegative() {
+    display.textContent = display.textContent.charAt(0) === '-' ?
+        display.textContent.slice(1) :
+        '-' + display.textContent;
+    triggerTextBlinkEffect();
+}
+
+function clearDisplay() {
+    display.textContent = '0';
+}
+
+function addDecimal(text) {
+    if (!display.textContent.includes(text)) {
+        addText(text);
+    }
+}
+
+function displayText(text) {
+    if (numbers.includes(text)) {
+        display.textContent === '0' ? display.textContent = text : addText(text);
+    } else if (text === '.' ) {
+        addDecimal(text);
+    } else if (text === '+/-') {
+        if (display.textContent !== '0') {
+            toggleNegative();
+        }
+    } else if (text === 'C') {
+        clearDisplay();
+    } else {
+        triggerTextBlinkEffect();
     }
 }
